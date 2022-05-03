b01e20dcad2d2541aa6877003d054c907cd66179
Ben <103605021+BP0201@users.noreply.github.com>
2022-05-03 16:19:00 -0400

    added web exercise

---
+++ 17.1-web-ex/app.js
@@ -0,0 +1,9 @@
+// HTTP stands for hypertext transfer protocol. it is one of many protocols, and its used to send or receive data to a server
+// a URL is a universal resource locator, it is the human readable link that is then broken down and interpreted, which allows you to visit the link
+// DNS is domain name system, this is what converts the URL and breaks it down into numbers for the computer to read
+// a query string is the last part of the URL, where you see different values that have been submitted, some automatically. This looks like ?x=1&y=2
+// GET and POST. They are used to request/send data, GET does not change any data on the server, however POST will. 
+// an HTTP request is the request you would make with these verbs. they are used to request/send data.
+// the HTTP response is what you get back after making any type of request.
+// an HTTP header specifies what you are looking for from the request. Some request headers are Accept and User-Agent. An example response header is Content-Type
+// When you type a URL into the browser, your computer searches for any previous visits to the URL, then turns it into an IP address using DNS. Then the server will send a response with the HTML of the website.
