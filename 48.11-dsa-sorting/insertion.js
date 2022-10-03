c06a4db7738cf5257e5f106ae2fce97196123baf
Ben <103605021+BP0201@users.noreply.github.com>
2022-10-03 18:27:33 -0400

    added 48.11-ex

---
+++ 48.11-dsa-sorting/insertion.js
@@ -0,0 +1,17 @@
+function insertionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let swapped = false;
+        for (let j = 1; j < arr.length - i; j++) {
+            if (arr[j] < arr[j - 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+    return arr;
+}
+
+module.exports = insertionSort;
---
+++ 48.11-dsa-sorting/selection.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+    // for (let i = 0; i < arr.length; i++) {
+    //     let min = arr[i]
+    //     for (let j = i; j < arr.length; j++) {
+    //         if (min > arr[j]) {
+    //             min = arr[j]
+    //         }
+    //     }
+    //     if (min !== arr[i]) {
+    //         let temp = arr[i]
+    //         arr[i] = min
+    //         min = temp
+    //     }
+    // }
+    // return arr;
+
+    // does not work
+}
+
+module.exports = selectionSort;
